01310717cdd337a9f4aa0aa056683072c215029a
LOGIC50 <ahmedjafrulislam@gmail.com>
2021-10-18 09:07:23 +0600

    first commit

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDE5uPprVETtLDKxiroDHUke6HpyFZv5S8",
+    authDomain: "imtiaz-hospital-and-lab.firebaseapp.com",
+    projectId: "imtiaz-hospital-and-lab",
+    storageBucket: "imtiaz-hospital-and-lab.appspot.com",
+    messagingSenderId: "969237284852",
+    appId: "1:969237284852:web:3916e972419c0fd1741ba1"
+  };
+
+  export default firebaseConfig;
